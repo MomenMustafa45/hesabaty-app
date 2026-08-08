4f5f037ca1a69abecc04c21b69e993f422115f2d
Momen Mustafa <momenmustafa371@gmail.com>
2026-08-08 12:22:33 +0300

    chore: initialize project folder structer

---
+++ babel.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.jsx', '.ts', '.tsx', '.json'],
+        alias: {
+          '@components': './src/components',
+          '@features': './src/features',
+          '@navigations': './src/navigations',
+          '@lib': './src/lib',
+          '@config': './src/config',
+          '@types': './src/types',
+          '@assets': './src/assets',
+          '@hooks': './src/hooks',
+          '@store': './src/store',
+          '@providers': './src/providers',
+          '@storage': './src/storage',
+          '@locales': './src/locales',
+        },
+      },
+    ],
+  ],
+};
---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  preset: '@react-native/jest-preset',
+  moduleNameMapper: {
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@features/(.*)$': '<rootDir>/src/features/$1',
+    '^@navigations/(.*)$': '<rootDir>/src/navigations/$1',
+    '^@lib/(.*)$': '<rootDir>/src/lib/$1',
+    '^@config/(.*)$': '<rootDir>/src/config/$1',
+    '^@types/(.*)$': '<rootDir>/src/types/$1',
+    '^@assets/(.*)$': '<rootDir>/src/assets/$1',
+    '^@hooks/(.*)$': '<rootDir>/src/hooks/$1',
+    '^@store/(.*)$': '<rootDir>/src/store/$1',
+    '^@providers/(.*)$': '<rootDir>/src/providers/$1',
+    '^@storage/(.*)$': '<rootDir>/src/storage/$1',
+    '^@locales/(.*)$': '<rootDir>/src/locales/$1',
+  },
+};
